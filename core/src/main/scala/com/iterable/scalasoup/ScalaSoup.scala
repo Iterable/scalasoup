35b42f5beae0c75fe184dddffe2337cef3e0d750
jon.mcpherson <jon.mcpherson@iterable.com>
2023-04-25 15:09:51 -0700

    Add missing static parse function overloads

--- core/src/main/scala/com/iterable/scalasoup/ScalaSoup.scala
+++ core/src/main/scala/com/iterable/scalasoup/ScalaSoup.scala
@@ -1,30 +1,60 @@
 package com.iterable.scalasoup
 
+import org.jsoup.Jsoup
+import org.jsoup.parser.Parser
+
+import java.io.{File, InputStream}
+import java.net.URL
+
 object ScalaSoup {
+
+  def parse(html: String): Document[ParentState.NoParent] = new Document(Jsoup.parse(html))
+
   def parse(html: String, baseUri: String): Document[ParentState.NoParent] =
-    new Document(org.jsoup.Jsoup.parse(html, baseUri))
+    new Document(Jsoup.parse(html, baseUri))
 
-  def parse(html: String, baseUri: String, parser: org.jsoup.parser.Parser): Document[ParentState.NoParent] =
-    new Document(org.jsoup.Jsoup.parse(html, baseUri, parser))
+  def parse(html: String, parser: Parser): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(html, parser))
 
-  def parse(html: String): Document[ParentState.NoParent] = new Document(org.jsoup.Jsoup.parse(html))
+  def parse(html: String, baseUri: String, parser: Parser): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(html, baseUri, parser))
+
+  def parse(file: File): Document[ParentState.NoParent] = new Document(Jsoup.parse(file))
+
+  def parse(file: File, charsetName: String): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(file, charsetName))
+
+  def parse(file: File, charsetName: String, baseUri: String): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(file, charsetName, baseUri))
+
+  def parse(file: File, charsetName: String, baseUri: String, parser: Parser): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(file, charsetName, baseUri, parser))
+
+  def parse(in: InputStream, charsetName: String, baseUri: String): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(in, charsetName, baseUri))
+
+  def parse(in: InputStream, charsetName: String, baseUri: String, parser: Parser): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(in, charsetName, baseUri, parser))
+
+  def parse(url: URL, timeoutMillis: Int): Document[ParentState.NoParent] =
+    new Document(Jsoup.parse(url, timeoutMillis))
 
   def parseBodyFragment(bodyHtml: String, baseUri: String): Document[ParentState.NoParent] =
-    new Document(org.jsoup.Jsoup.parseBodyFragment(bodyHtml, baseUri))
+    new Document(Jsoup.parseBodyFragment(bodyHtml, baseUri))
 
   def parseBodyFragment(bodyHtml: String): Document[ParentState.NoParent] =
-    new Document(org.jsoup.Jsoup.parseBodyFragment(bodyHtml))
+    new Document(Jsoup.parseBodyFragment(bodyHtml))
 
   def clean(bodyHtml: String, baseUri: String, safelist: Safelist): String =
-    org.jsoup.Jsoup.clean(bodyHtml, baseUri, safelist.underlying)
+    Jsoup.clean(bodyHtml, baseUri, safelist.underlying)
 
   def clean(bodyHtml: String, safelist: Safelist): String =
-    org.jsoup.Jsoup.clean(bodyHtml, safelist.underlying)
+    Jsoup.clean(bodyHtml, safelist.underlying)
 
   def clean(bodyHtml: String, baseUri: String, safelist: Safelist, outputSettings: OutputSettings): String =
-    org.jsoup.Jsoup.clean(bodyHtml, baseUri, safelist.underlying, outputSettings.underlying)
+    Jsoup.clean(bodyHtml, baseUri, safelist.underlying, outputSettings.underlying)
 
-  def isValid(bodyHtml: String, safelist: Safelist): Boolean = org.jsoup.Jsoup.isValid(bodyHtml, safelist.underlying)
+  def isValid(bodyHtml: String, safelist: Safelist): Boolean = Jsoup.isValid(bodyHtml, safelist.underlying)
 
   private[scalasoup] def withClone[A <: java.lang.Cloneable, B](underlying: A)(clone: A => A)(ctor: A => B)(op: A => Unit): B = {
     val c = clone(underlying)
